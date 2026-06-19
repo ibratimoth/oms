6e8165a6d991ae810b7b05a06f2497aa89fb88a5
ibratimoth <ibratimoth@gmail.com>
2026-06-20 00:07:20 +0300

    first commit

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -1,13 +1,67 @@
+// const { Product, OrderItem, sequelize } = require('../models');
+
+// exports.index = async (req, res) => {
+
+//   try {
+
+//     const products = await Product.findAll();
+
+//     const stockData = await Promise.all(products.map(async (p) => {
+
+//       const sold = await OrderItem.sum('quantity', {
+//         where: { product_id: p.id }
+//       });
+
+//       return {
+//         id: p.id,
+//         name: p.name,
+//         stock: p.quantity_in_stock + (sold || 0),
+//         sold: sold || 0,
+//         remaining: p.quantity_in_stock,
+//         reorder_level: p.reorder_level
+//       };
+//     }));
+
+//     const topProducts = await OrderItem.findAll({
+//       attributes: [
+//         'product_id',
+//         [sequelize.fn('SUM', sequelize.col('quantity')), 'total_sold']
+//       ],
+//       include: [{
+//         model: Product,
+//         required: true
+//       }],
+//       group: ['product_id', 'Product.id'],
+//       order: [[sequelize.literal('total_sold'), 'DESC']],
+//       limit: 10
+//     });
+
+//     res.render('analytics/index', {
+//       stockData,
+//       topProducts
+//     });
+
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Analytics error');
+//   }
+// };
+
 const { Product, OrderItem, sequelize } = require('../models');
 
 exports.index = async (req, res) => {
-
   try {
+    const userId = req.session.user.id;
 
-    const products = await Product.findAll();
+    const userFilter = {
+      created_by: userId 
+    };
+
+    const products = await Product.findAll({
+      where: userFilter
+    });
 
     const stockData = await Promise.all(products.map(async (p) => {
-
       const sold = await OrderItem.sum('quantity', {
         where: { product_id: p.id }
       });
@@ -29,7 +83,8 @@ exports.index = async (req, res) => {
       ],
       include: [{
         model: Product,
-        required: true
+        required: true,
+        where: userFilter 
       }],
       group: ['product_id', 'Product.id'],
       order: [[sequelize.literal('total_sold'), 'DESC']],
